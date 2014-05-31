ac4e3279a5defb21da906c6f4fe699513b76216a
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-06-01 05:28:48 +0530

    Give sentences that have a score of <=2 a score of 0

--- src/annotate/Annotator.java
+++ src/annotate/Annotator.java
@@ -10,44 +10,59 @@ import delic.Document;
 import delic.Sentence;
 
 /**
- * This is the workhorse, responsible for annotating the 
- * given document. Gets a document and returns an arraylist of
+ * This is the workhorse, responsible for annotating the given document. Gets a
+ * document and returns an arraylist of
+ * 
  * @author aman
- *
+ * 
  */
 public class Annotator {
 
 	ArrayList<Concept> conceptList;
-	
+
 	public Annotator(ArrayList<Concept> concepts) {
 		this.conceptList = concepts;
 	}
-	
+
 	/**
-	 * This function receives a document and returns a list of annotated sentences
-	 * Where the annotated sentence is as defined by the class delic.AnnotatedSentence 
+	 * This function receives a document and returns a list of annotated
+	 * sentences Where the annotated sentence is as defined by the class
+	 * delic.AnnotatedSentence
+	 * 
 	 * @param licenseDoc
 	 * @return
 	 */
 	public ArrayList<AnnotatedSentence> annotateDoc(Document licenseDoc) {
 		ArrayList<AnnotatedSentence> annotations = new ArrayList<AnnotatedSentence>();
-		//Iterator<Sentence> sentenceItr = licenseDoc.getSentencePOSBased();
+		// Iterator<Sentence> sentenceItr = licenseDoc.getSentencePOSBased();
 		Iterator<Sentence> sentenceItr = licenseDoc.getSentenceIterator();
-		while(sentenceItr.hasNext()) { //for each sentence
-			ArrayList<Concept> matchedConceptList = new ArrayList<Concept>(); //maintain a list of concepts that are matched
+		while (sentenceItr.hasNext()) { // for each sentence
+			ArrayList<Concept> matchedConceptList = new ArrayList<Concept>(); // maintain
+																				// a
+																				// list
+																				// of
+																				// concepts
+																				// that
+																				// are
+																				// matched
 			Sentence currSentence = sentenceItr.next();
-			for(Concept c : this.conceptList) {
-				if(c.isContainedIn(currSentence)) {
+			for (Concept c : this.conceptList) {
+				if (c.isContainedIn(currSentence)) {
 					matchedConceptList.add(c);
 				}
 			}
 			int score = ScoreAssigner.getNaiveScore(matchedConceptList);
-			annotations.add(new AnnotatedSentence(currSentence, matchedConceptList, score));
+			if (score > 2) {
+				annotations.add(new AnnotatedSentence(currSentence,
+						matchedConceptList, score));
+			} else {
+				annotations.add(new AnnotatedSentence(currSentence,
+						matchedConceptList, 0));
+
+			}
+
 		}
 		return annotations;
 	}
-	
-	
-	
-	
+
 }
